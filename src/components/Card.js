b9009a12955209f8758a86f9b282ddb1ec7f868d
HanmaDaiki <daikihanma@outlook.com>
2022-08-04 20:34:56 +0300

    add: Логика лайков и удаления!

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,24 @@
 import React from "react";
+import {CurrentUserContext} from "../contexts/CurrentUserContext";
 
-function Card({ card, onCardClick }) {
+function Card({ card, onCardClick, onCardLike, onDeleteCard }) {
   function handleClick() {
     onCardClick(card);
   }
+
+  function handleLikeClick() {
+    onCardLike(card);
+  }
+
+  function handleDeleteClick() {
+    onDeleteCard(card);
+  }
+
+  const user = React.useContext(CurrentUserContext);
+  const isOwn = (user.id === card.owner._id);
+  const isLiked = card.likes.some(like => like._id === user.id);
+  const cardLikeButtonClass = `${isLiked ? 'card__like-button card__like-button_active' : 'card__like-button'}`
+
   return (
     <article className="card">
       <img
@@ -14,10 +29,13 @@ function Card({ card, onCardClick }) {
       />
       <h2 className="card__title">{card.name}</h2>
       <div className="card__like">
-        <button className="card__like-button"></button>
+        <button className={cardLikeButtonClass} onClick={handleLikeClick}></button>
         <span className="card__like-counter">{card.likes.length}</span>
       </div>
-      <button className="card__delete"></button>
+      {
+        isOwn &&
+        <button className="card__delete" onClick={handleDeleteClick}></button>
+      }
     </article>
   );
 }
